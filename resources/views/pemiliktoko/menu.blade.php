17f4ae2710cd29d563255f60a060b82bc62dd5f1
Brielt Bella Gracitwo <44756651+EGraci@users.noreply.github.com>
2022-06-20 11:12:01 +0700

    remove min js

--- resources/views/pemiliktoko/menu.blade.php
+++ resources/views/pemiliktoko/menu.blade.php
@@ -158,8 +158,8 @@
                 <!-- End of Topbar -->
                 
                 <!-- Bootstrap core JavaScript-->
-                <script src="vendor/jquery/jquery.min.js"></script>
-                <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                <script src="vendor/jquery/jquery.js"></script>
+                <script src="vendor/bootstrap/js/bootstrap.bundle.js"></script>
 
                 {{-- jquery --}}
                 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
@@ -169,7 +169,7 @@
 
                 
                 <!-- Custom scripts for all pages-->
-                <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+                <script src="{{asset('js/sb-admin-2.js')}}"></script>
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     @yield('konten')
